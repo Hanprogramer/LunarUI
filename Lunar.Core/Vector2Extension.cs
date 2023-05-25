3b321fee5249b049bc7d44773aedb1d9f07c8e4d
Han Programer <30620423+Hanprogramer@users.noreply.github.com>
2023-05-25 11:51:52 +0700

    Removed the custom Vector2 code

---
+++ Lunar.Core/Vector2Extension.cs
@@ -0,0 +1,23 @@
+﻿// using Silk.NET.Maths;
+
+global using System.Numerics;
+namespace Lunar.Native
+{
+    public static class Vector2Extension
+    {
+        public static Vector2 WithX(this Vector2 a, float x)
+        {
+            return a with
+            {
+                X = x
+            };
+        }
+        public static Vector2 WithY(this Vector2 a, float y)
+        {
+            return a with
+            {
+                Y = y
+            };
+        }
+    }
+}
--- Lunar/Lunar.cs
+++ Lunar/Lunar.cs
@@ -1,4 +1,5 @@
-﻿namespace Lunar
+﻿global using System.Numerics;
+namespace Lunar
 {
     public class Lunar
     {
