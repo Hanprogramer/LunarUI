fe21bb2552da3fd58d630744cacf612e0ac69707
Han Programer <30620423+Hanprogramer@users.noreply.github.com>
2023-08-19 16:34:11 +0700

    Update Program.cs

--- Lunar.Runner/Program.cs
+++ Lunar.Runner/Program.cs
@@ -2,10 +2,7 @@
 
 using Lunar;
 using Lunar.Scripting;
-var projectDir = @"D:\Programming\C#\Lunar\CoreLauncher";
-Console.WriteLine("Running test project...");
-
-var app = new Application(projectDir, "CoreLauncher");
+var app = new Application(args[0], "CoreLauncher");
 app.AddWindowFeature<XmlScriptingFeature>();
 app.AddFeature(new V8ScriptingFeature());
 app.CreateRootWindow();
